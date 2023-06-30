ed2433143dfc6ce7d34f32cc4247215f24c06c0b
Joshua Ugwuede <joshuaugwuede@gmail.com>
2023-06-30 19:58:04 +0100

    build correct

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,17 @@
+pip install -r requirements.txt
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic --noinput
 
-echo "Building Project..."
-python3.9 -m pip install -r requirements.txt
 
-echo "Making Migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 
-echo "Collecting Static Files for Production..."
-python3.9 manage.py collectstatic --noinput --clear
+
+
+# echo "Building Project..."
+# python3.9 -m pip install -r requirements.txt
+
+# echo "Making Migrations..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
+
+# echo "Collecting Static Files for Production..."
+# python3.9 manage.py collectstatic --noinput --clear
