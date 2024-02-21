6ecf5273c6a494f503ee586bdf56b063172b27ef
Joshua <joshuaugwuede@gmail.com>
2024-02-21 19:22:32 +0100

    good

--- frontend/src/components/EditorComp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditorComp = () => {
-  return <div></div>;
-};
-
-export default EditorComp;
--- frontend/src/components/createCategory.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const createCategory = () => {
-  return <div></div>;
-};
-
-export default createCategory;
